fe44902a43ad48ee634badaed62c8465511d595c
1397457773 <1397457773@qq.com>
2017-08-14 20:13:13 +0800

    第二次提交 框架内容填充，新建newMethod方法
    时间：2017年8月14日20:12:31
    程序员：李俊岐

--- app/src/main/java/com/jiyun/githab_test4/Demo.java
+++ app/src/main/java/com/jiyun/githab_test4/Demo.java
@@ -1,8 +1,13 @@
 package com.jiyun.githab_test4;
 
+import android.util.Log;
+
 /**
  * Created by Lenovo on 2017/8/14.
  */
 
 public class Demo {
+    public void newMethod(){
+        Log.e("TAG","Demo");
+    }
 }
